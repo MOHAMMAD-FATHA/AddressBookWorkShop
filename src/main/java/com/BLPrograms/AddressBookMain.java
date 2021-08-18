4013d09f960db18d3d4812a2b5048dd7618f861f
unknown <mfjkd7795@gmail.com>
2021-08-17 20:34:14 -0700

    Added : Method to create contacts in address book

---
+++ src/main/java/com/BLPrograms/AddressBookMain.java
@@ -0,0 +1,46 @@
+package com.BLPrograms;
+
+import java.util.Scanner;
+
+public class AddressBookMain {
+
+    /*
+     * @author - Mohammad Fatha
+     * Purpose to add the details of the person using collection
+     */
+
+        public static void main(String[] args)
+        {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter how many contacts you want to store at a time : ");
+            int enterCount = sc.nextInt();
+            for (int i = 0; i < enterCount; i++) {
+
+                Person contactDetails = new Person();
+                System.out.print("Enter First Name : ");
+                String firstName = sc.next();
+                contactDetails.setFirstName(firstName);
+
+                System.out.print("Enter Last Name : ");
+                contactDetails.setLastName(sc.next());
+
+                System.out.print("Enter Address : ");
+                contactDetails.setAddress(sc.next());
+
+                System.out.print("Enter City : ");
+                contactDetails.setCity(sc.next());
+
+                System.out.print("Enter State : ");
+                contactDetails.setState(sc.next());
+
+                System.out.print("Enter ZipCode : ");
+                contactDetails.setZipCode(sc.nextInt());
+
+                System.out.print("Enter Phone-Number : ");
+                contactDetails.setPhoneNumber(sc.nextLong());
+
+                System.out.print("Enter Email-Id : ");
+                contactDetails.setEmailId(sc.next());
+            }
+        }
+}
